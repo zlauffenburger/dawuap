0cc89e608d2c70b1eb8d215138b90c5ffb91a3a8
Marco Maneta <marco.maneta@umontana.edu>
2016-06-14 13:25:10 -0600

    some progress in snow component

--- hydroengine/rrmodels.py
+++ hydroengine/rrmodels.py
@@ -7,32 +7,50 @@ class hbv(object):
 
     """
 
-    def __init__(self, ):
+    def __init__(self, swe_o, pond_o, **params):
 
+        self.t_thres = params['pp_temp_thres']
+        self.p_base = params['p_base']
+        self.ddf = params['ddf']
+        self.swe = swe_o
+        self.pond = pond_o
 
-        self.t_thres = 2
-        self.p_base = 10
+    def snowpack(self, incid_precip, t_max, t_min):
 
-        pass
+        rain = 0
+        if (t_max <= self.t_thres):
+            swe = incid_precip
+            rain = 0
+        elif (t_min > self.t_thres):
+            rain = incid_precip
+            swe = 0
+        else:
+            swe = incid_precip * np.array(self.t_thres - t_min / (t_max - t_min)).clip(min=0)
 
+        self.swe += swe
 
-    def snowpack(self, incid_precip, temp, params):
+        tp = 0.5 * (t_max + t_min)
 
-         if(t_max < self.t_thres):
-             self.snow = incid_precip
-         else if(tmax)
+        self.melt[np.greater(tp, self.t_thres)]
 
-         if(temp > self.Ts):
+        if (tp > self.t_thres):
+            if (self.swe > 0):
+                self.melt = self.ddf * (tp - self.t_thres)
+                self.swe -= self.melt
+                if (self.melt > self.swe):
+                    self.melt = self.swe
+                    self.swe = 0
 
-             self.snow = self.ddf * (temp - self.Ts)
-         else:
-             self.melt = 0;
+        else:
+            self.melt = 0;
+
+        self.pond += self.melt + rain
 
     def excess_precip_to_runoff(self):
         def u(i):
-            return np.array(- ((self.p_base - 2 * i + 2) * np.abs(self.p_base - 2 * i + 2) + (2 * i - self.p_base) * np.abs(
-                2 * i - self.p_base) - 4 * self.p_base) / (2 * self.p_base ^ 2)).clip(min=0)
-
+            return np.array(
+                - ((self.p_base - 2 * i + 2) * np.abs(self.p_base - 2 * i + 2) + (2 * i - self.p_base) * np.abs(
+                    2 * i - self.p_base) - 4 * self.p_base) / (2 * self.p_base ^ 2)).clip(min=0)
 
         v = np.array([0, 0, 0, 0, 2, 3, 0, 2.3, 1.4, 2, 0, 0, 0, 0, 0, 0.2, 1.2, 2.2, 1.5, 0, 0, 0, 0])
         Q = np.convolve(v, u(np.arange(1, 10)))
--- tests/test_rrmodels.py
+++ tests/test_rrmodels.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 from .context import hydroengine
 import numpy as np
 import nose
@@ -8,16 +10,31 @@ class Test_hbv(object):
     @classmethod
     def setup_class(klass):
         print "SETUP!"
+        args = {
+            'pp_temp_thres': 2,
+            'p_base': 10,
+            'ddf': 0.02
+        }
+        Test_hbv.swe_o = np.zeros((20, 30))
+        Test_hbv.pond_o = np.zeros_like(Test_hbv.swe_o)
+
+        Test_hbv.ohbv = hydroengine.hbv(Test_hbv.swe_o, Test_hbv.pond_o, **args)
 
     @classmethod
     def teardown_class(klass):
         print "TEAR DOWN!"
-        plt.close()
 
     def TestInitialization(self):
-        Q = hydroengine.hbv().excess_precip_to_runoff()
-        plt.plot(Q)
-        plt.show()
+        np.testing.assert_array_equal(Test_hbv.swe_o, self.ohbv.swe)
+        nose.tools.assert_equals(self.ohbv.p_base, 10)
+        nose.tools.assert_equals(self.ohbv.t_thres, 2)
+        nose.tools.assert_equals(self.ohbv.ddf, 0.02)
 
-    def Test
+    def test_snowpack(self):
+        precip = np.ones_like(Test_hbv.swe_o) * 0.03
+        t_max = np.ones_like(Test_hbv.swe_o) * -2
+        t_min = np.ones_like(Test_hbv.swe_o) * -3
+        Test_hbv.ohbv.snowpack(precip, t_max, t_min)
+
+        np.testing.assert_array_equal(self.ohbv.swe, np.ones_like(Test_hbv.swe_o)*0.03)
 
