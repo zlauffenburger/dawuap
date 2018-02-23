432d9b9c75bf81cfc03b44bfbb431e4388a72a5b
Marco Maneta <marco.maneta@umontana.edu>
2018-02-23 00:15:09 -0500

    added new economic model functions and tests

--- econengine/econfuncs.py
+++ econengine/econfuncs.py
@@ -30,16 +30,43 @@ class Farm(WaterUser):
                              'for farm %i with name %s failed' % (self.id, self.name))
 
         # Check calibration criteria 2
-        b = np.sqrt(xbar)/(p * qbar)
+        b = (xbar)**2/(p * qbar)
         psi = self.sigma*ybar_w / (self.eta * (1 - ybar_w))
         ind = np.arange(len(b))
         cc2 = b * self.eta * (1 - psi) - [np.sum(b[ind != i] *
-                                                 self.eta[ind != i] * np.sqrt(1 + (1 / self.eta[ind != i])) *
+                                                 self.eta[ind != i] * (1 + (1 / self.eta[ind != i]))**2 *
                                                  (1 + psi[ind != i] - ybar_w[ind != i])) for i in ind]
         if (cc2 > 0).any():
-            print cc2
             raise ValueError('calibration criteria 2'
                              'for farm %i with name %s failed' % (self.id, self.name))
+
+    def _eta_sim(self, delta, xbar, ybar_w, qbar, p):
+        """
+        Simulated exogenous supply elasticities (eta) as a function of observed and prescribed parameters
+
+        :param delta: CES production function parameter, 1D array
+        :param xbar: Observed resource allocations, 2D array
+        :param ybar_w: observed
+        :param qbar:
+        :param p:
+        :return:
+        """
+
+        b = (xbar)**2 / (p * qbar)
+        num = b / (delta * (1 - delta))
+        dem = np.sum(num + self.sigma*b*ybar_w/(delta * (delta - ybar_w)))
+        return delta / (1 - delta) * (1 - (num/dem))
+
+    def _y_bar_w_sim(self, beta, delta, xbar):
+        """
+        Simualted yield elasticity (ybar_w) with respect to water
+
+        :return:
+        """
+        r = rho(self.sigma)
+        num = beta[:, -1] * xbar[:, -1]**r
+        den = np.diag(np.dot(beta, xbar.T**r))
+        return delta * num/den
 
     def simulate(self):
         pass
--- tests/test_econfuncs.py
+++ tests/test_econfuncs.py
@@ -24,7 +24,11 @@ class TestFarm(object):
         cls.eta = [.35, 0.29, 0.29, 1.33, 0.38, 0.38, 0.35, 0.35]
         cls.sigma = 0.5
 
-        cls.obs_land = 639.5
+        cls.obs_land = [0.1220, 0.0250, 0.0078, 0.0328, 0.1636, 0.0051, 0.0189, 0.6247]
+        cls.et0 = 5
+        cls.obs_water = np.array([0, 0, 27.25, 0, 0, 20., 0, 0]) + cls.et0
+        cls.xbar = np.array([cls.obs_land, cls.obs_water]).T
+
         cls.ybar = [35, 2.2, 5.4, 1.7, 30, 110, 36, 36]
         cls.p = [5.12, 134, 134, 10.5, 4.76, 4.76, 5.93, 3.88]
 
@@ -65,9 +69,39 @@ class TestFarm(object):
         nose.tools.assert_raises(ValueError,
                                  TestFarm.a._check_calibration_criteria(
                                      np.array(TestFarm.obs_land),
-                                     np.array(TestFarm.ybar_w),
+                                     np.array(TestFarm.ybar_w)*10,
                                      np.array(TestFarm.qbar),
                                      np.array(TestFarm.p)))
+
+    def test_eta_sim(self):
+        # tests function eta for calibrated delta values such that simulated eta with given
+        # delta produces observed eta
+        delta = [0.276, 0.227, 0.225, 0.620,0.304, 0.276, 0.261, 0.387]
+        np.testing.assert_allclose(TestFarm.a._eta_sim(
+            np.array(delta),
+            np.array(TestFarm.obs_land),
+            np.array(TestFarm.ybar_w),
+            np.array(TestFarm.qbar),
+            np.array(TestFarm.p)),
+            TestFarm.eta)
+
+    def test_y_bar_w_sim(self):
+        # tests function eta for calibrated delta values such that simulated eta with given
+        # delta produces observed eta
+        beta = [[0.418, 0.582],
+                [0.016, 0.984],
+                [0.002, 0.998],
+                [0.217, 0.783],
+                [0.448, 0.552],
+                [0.126, 0.874],
+                [0.402,0.598],
+                [0.522, 0.478]]
+        delta = [0.276, 0.227, 0.225, 0.620,0.304, 0.276, 0.261, 0.387]
+        np.testing.assert_allclose(TestFarm.a._y_bar_w_sim(
+            np.array(beta),
+            np.array(delta),
+            np.array(TestFarm.xbar)),
+            TestFarm.ybar_w)
 
 
 
