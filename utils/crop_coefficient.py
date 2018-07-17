dbd724a461d3a72328b3e8944c9e21f1a1bfec40
Marco Maneta <marco.maneta@umontana.edu>
2018-07-17 09:49:21 -0600

    added doc string to utils.crop_coefficients.retrieve_crop_coefficient function

--- utils/crop_coefficient.py
+++ utils/crop_coefficient.py
@@ -10,7 +10,24 @@ DATA_PATH = pkg_resources.resource_filename('utils', '/')
 
 def retrieve_crop_coefficient(current_date, start_date, cover_date, end_date,
                               crop_id, kc_table="crop_coefficients.txt"):
-    "Returns crop coefficient for current_date interpolated from agMet lookup table"
+    """Returns crop coefficient for current_date interpolated from agMet lookup table. Dates are strings
+    with m/d/YYYY format if they are ambiguous (e.g. 06/03/12 is June, 3 2012)
+
+    Parameters
+    ==========
+
+    :param current_date: Date for crop coefficient, m/d/YYYY string if dates are ambiguous
+    :param start_date: Crop planting date, m/d/YYYY string if dates are ambiguous
+    :param cover_date: Date at which crop is fully matured and has reached maximum coverage
+    :param end_date: Date at which crop ends, either harvested or dead
+    :param crop_id: Integer with crop id from AgMet crop coefficient lookup table
+    :param kc_table: Optional. Text lookup with crop coefficient curves. See default table for format.
+
+    Returns
+    =======
+    :returns: float, crop coefficient of crop_id for corresponding current_date
+
+    """
 
     df_kc = pd.read_table(DATA_PATH + kc_table, index_col="crop_id")
     current_date = parser.parse(current_date)
